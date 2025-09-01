305be4f440b6e8b429a38175c2620e6a9ab9f088
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-01 17:01:32 +0000

    Implemented Todo Entity

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,8 +1,66 @@
 /*
+ *
  * You can use the following import statements
- * 
+ *
  * import javax.persistence.*;
- * 
+ *
  */
+package com.example.todo.model;
 
-// Write your code here
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todolist")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String todo;
+    private String status;
+    private String priority;
+
+    public Todo() {
+    }
+
+    public Todo(int id, String todo, String status, String priority) {
+        this.id = id;
+        this.todo = todo;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
