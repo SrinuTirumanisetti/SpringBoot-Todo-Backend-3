f610cdf7ac5ce2d39df038dee74ea7c95201a49d
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-18 16:56:00 +0000

    Implement Abstract methods for Todo

--- src/main/java/com/example/todo/repository/TodoRepository.java
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -1,1 +1,11 @@
-// Write your code here
+package com.example.todo.repository;
+
+import com.example.todo.model.Todo;
+import java.util.*;
+
+public interface TodoRepository {
+    List<Todo> findAll();
+    Todo save(Todo todo);
+    Optional<Todo> findById(int id);
+    void deleteById(int id);
+}
